2227aa836016be6c4aee4393318c2a2a3d65e00b
Dian956 <dianamwelu554@gmail.com>
2025-05-27 12:35:45 +0300

    coding

---
+++ Web design/Programming.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int maain({
+
+    printf("Hello team?\n");
+    printf("Welcome to introduction to programming class\n");
+    printf("We will be programming with C language\n");
+    printf("I hope we have fun coding together.");
+    printf("All the best!!");
+
+    return 0;
+}
